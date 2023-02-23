e082a1a43ee8e108d55ac205ac8a7f9cd30d98d1
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-02-24 11:09:20 +1300

    first functions, styles, vars

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,47 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<!-- CSS Styling -->
+<style>
+    h1
+    {
+        color:blue;
+        font-size:14px;
+        font-style:italic;
+        font-weight:normal;
+    }
+    button
+    {
+        color:black;
+        font-size:14px;
+        font-style:italic;
+        font-weight:normal;    
+        background-color:white;  
+    }
+</style>
+<!-- JS  -->
+<script>
+    const num = 21;
+    let first_name = "Jay";
+
+    let count = 0;
+
+    function incrementCount()
+    {
+        count++;
+    }
+
+    function reset() 
+    {
+        count = 0;
+    }
+</script>
+
+<button on:click={incrementCount}>
+    Clicked {count} {count === 1 ? 'time' : 'times'}
+</button>
+
+<button on:click={reset}>
+    reset
+</button>
+
+<h1>Welcome to SvelteKit {first_name}</h1>
+
 <p> Hello </p>
