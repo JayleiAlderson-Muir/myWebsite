8d3da0326523618114c099ad5d679019af61b6e5
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-20 10:00:12 +1200

    add table

--- src/routes/page2/+page.svelte
+++ src/routes/page2/+page.svelte
@@ -8,17 +8,54 @@
     const response = await get();
     html = response.body;
   });*/
+  import SvelteTable from "svelte-table";
+
   import {switchData} from "$lib/switchData.js";
   let keys = Object.keys(switchData[0]);
   console.log(keys);
+  let sortBy = "Brand";
+  let sortOrder = 1;
+  const rows = switchData;
+  const columns = [
+    {key: "Brand", title: "Brand", value: v => v.Brand, sortable: true,},
+    {key: "Name", title: "Name", value: v => v.Name, sortable: true,},
+    {key: "Type", title: "Type", value: v => v.Type, sortable: true,},
+    {key: "ActuationForce", title: "Actuation Force", value: v => v.ActuationForce, sortable: true,},
+    {key: "ActuationDistance", title: "Actuation Distance", value: v => v.ActuationDistance, sortable: true,},
+    {key: "BottomOutForce", title: "Bottom Out Force", value: v => v.BottomOutForce, sortable: true,},
+    {key: "TravelDistance", title: "Bottom Out Travel", value: v => v.TravelDistance, sortable: true,},
+    {key: "StemType", title: "Stem Compatibility", value: v => v.StemType, sortable: true,},
+    {key: "PrebuiltAvailability", title: "Available in prebuilt", value: v => v.PrebuiltAvailability, sortable: true,}
+  ]
+  function sortList(key) {
+    switchData.sort((a,b) => {
+      if (typeof a[key] == "string" && typeof b[key] == "string")
+        if (a[key].toLowerCase() > b[key].toLowerCase())
+          return -1;
+        else return 1;
+      else 
+        if (a[key] > b[key])
+          return -1;
+        else return 1;
+    })
+  }
+  sortList("ActuationForce")
+  console.log(switchData)
 </script>
 
   <div>
-    <ul>
-      <li>{switchData[0].Name}</li>
-      <li>{switchData[0].ActuationForce}</li>
-      <li><img src = {switchData[0].Image} alt = {switchData[0].Brand}/></li>
-    </ul>
+    <SvelteTable
+      columns={columns}
+      rows={rows}
+      bind:sortBy
+      bind:sortOrder
+      classNameTable={['table table-striped']}
+      classNameThead={['table-primary']}
+      classNameSelect={['custom-select']}
+      classNameRowSelected={'test'} />
 </div>
 
 
+<style>
+  
+</style>
