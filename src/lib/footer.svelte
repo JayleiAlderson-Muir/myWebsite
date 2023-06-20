4bf7bc30d3b4ed536c7b648bbcd03743f5b2c918
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-20 16:38:54 +1200

    feat: add fetch to footer

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,19 +1,25 @@
 <script>
-    import {page} from '$app/stores';
-    import Navigation from './navigation.svelte';
+  import { page } from "$app/stores";
+  import Navigation from "./navigation.svelte";
+  let url = "https://reqres.in";
+  fetch(url)
+    .then((response) => response.text())
+    .then((data) => {
+      console.log(data);
+    });
 </script>
 
-<footer id = "about">
-    <div>
-        <p>About</p>
-    </div>
+<footer id="about">
+  <div>
+    <p>About</p>
+  </div>
 </footer>
 
 <style>
-    footer {
-        display: flex;
-        justify-content: center;
-        margin: 0 auto;
-        width: 75%;
-    }
+  footer {
+    display: flex;
+    justify-content: center;
+    margin: 0 auto;
+    width: 75%;
+  }
 </style>
