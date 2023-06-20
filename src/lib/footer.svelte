699d0c9729e894216f422064942e58ad70796c46
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-21 11:57:48 +1200

    feat : add footer fetch

--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,12 +1,14 @@
 <script>
   import { page } from "$app/stores";
   import Navigation from "./navigation.svelte";
-  const BASE_URL = "https://api.unsplash.com/";
-  const AUTHORIZATION = "?client_id=Ca5OEr6brF6hFhF--FoVw0f_UnIGPP1CtPdnrjmp9Mo";
-  fetch(url)
+  const BASE_URL = "https://api.unsplash.com/search/photos";
+  const AUTHORIZATION = "client_id=Ca5OEr6brF6hFhF--FoVw0f_UnIGPP1CtPdnrjmp9Mo";
+  let keyboardData;
+  fetch(`${BASE_URL}?page=1&query=mechanical-keyboard&${AUTHORIZATION}`)
     .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      //console.log(data.results[0].urls.raw);
+      keyboardData = data.results;
     });
 </script>
 
