94537de4486fd7de75bc32863f671ac8645569b7
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-21 12:30:36 +1200

    feat : add footer cycling image

---
+++ src/lib/fadingImage.svelte
@@ -0,0 +1,39 @@
+<script>
+  const BASE_URL = "https://api.unsplash.com/search/photos";
+  const AUTHORIZATION = "client_id=Ca5OEr6brF6hFhF--FoVw0f_UnIGPP1CtPdnrjmp9Mo";
+  let keyboardData;
+  let keyboardImage;
+  let rawData;
+  let index = 0;
+  fetch(`${BASE_URL}?page=1&query=mechanical-keyboard&${AUTHORIZATION}`)
+    .then((response) => response.json())
+    .then((data) => {
+      //console.log(data.results[0].urls.raw);
+      rawData = data;
+      setInterval(changeImage, 5000);
+    });
+    
+    function changeImage() {
+        if (index >= rawData.results.length-1) {
+            index = 0;
+        }
+        keyboardData = rawData.results[index++];
+        keyboardImage = keyboardData.urls.raw;
+        
+    }
+    console.log(keyboardImage);
+</script>
+
+<section>
+    {#if keyboardImage}
+        <img src="{keyboardImage}"/>
+    {/if}
+</section>
+
+<style>
+    img {
+        width: 100vw;
+        
+        object-fit: cover;
+    }
+</style>
--- src/lib/footer.svelte
+++ src/lib/footer.svelte
@@ -1,20 +1,13 @@
 <script>
   import { page } from "$app/stores";
   import Navigation from "./navigation.svelte";
-  const BASE_URL = "https://api.unsplash.com/search/photos";
-  const AUTHORIZATION = "client_id=Ca5OEr6brF6hFhF--FoVw0f_UnIGPP1CtPdnrjmp9Mo";
-  let keyboardData;
-  fetch(`${BASE_URL}?page=1&query=mechanical-keyboard&${AUTHORIZATION}`)
-    .then((response) => response.json())
-    .then((data) => {
-      //console.log(data.results[0].urls.raw);
-      keyboardData = data.results;
-    });
+  import FadingImage from "./fadingImage.svelte";
 </script>
 
 <footer id="about">
   <div>
     <p>About</p>
+    <FadingImage/>
   </div>
 </footer>
 
