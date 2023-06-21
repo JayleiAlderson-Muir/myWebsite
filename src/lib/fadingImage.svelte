90e28184a29b23bbd67184e7f5434f8892042d45
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-21 18:03:15 +1200

    chore : clean code fadingImage component

--- src/lib/fadingImage.svelte
+++ src/lib/fadingImage.svelte
@@ -1,9 +1,7 @@
 <script>
   import { tweened } from "svelte/motion";
 
-  let imagePositionX;
   let imagePositionY;
-  //let imagePosition = 0;
   const BASE_URL = "https://api.unsplash.com/search/photos";
   const AUTHORIZATION = "client_id=Ca5OEr6brF6hFhF--FoVw0f_UnIGPP1CtPdnrjmp9Mo";
   let keyboardData;
@@ -13,18 +11,14 @@
   fetch(`${BASE_URL}?page=1&query=mechanical-keyboard&${AUTHORIZATION}`)
     .then((response) => response.json())
     .then((data) => {
-      //console.log(data.results[0].urls.raw);
       rawData = data;
-      setInterval(changeImage, 5100);
+      setInterval(changeImage, 20500);
     });
 
   function changeImage() {
-    if (!imagePositionX) {
-      imagePositionX = tweened(20, {
-        duration: 5000,
-      });
+    if (!imagePositionY) {
       imagePositionY = tweened(20, {
-        duration: 5000,
+        duration: 20000,
       });
     }
     if ($imagePositionY == 20) {
@@ -33,10 +27,8 @@
       }
       keyboardData = rawData.results[index++];
       keyboardImage = keyboardData.urls.raw;
-      imagePositionX.set(80);
       imagePositionY.set(80);
     } else {
-      imagePositionX.set(20);
       imagePositionY.set(20);
     }
   }
@@ -47,7 +39,7 @@
     <img
       src={keyboardImage}
       alt="keyboard"
-      style="--image-pos-x: {$imagePositionX}%;--image-pos-y: {$imagePositionY}%"
+      style="--image-pos-y: {$imagePositionY}%"
     />
   {/if}
 </section>
@@ -55,7 +47,7 @@
 <style>
   img {
     object-fit: cover;
-    object-position: var(--image-pos-x) var(--image-pos-y);
+    object-position: 50% var(--image-pos-y);
     overflow: hidden;
     height: 100%;
     width: 100%;
