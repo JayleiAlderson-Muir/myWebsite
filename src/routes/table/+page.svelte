c4974004ef1f23d46e7dba0247ad9651626179de
JayleiAlderson-Muir <aldej2@student.op.ac.nz>
2023-06-21 18:53:36 +1200

    adjust table page

--- src/routes/table/+page.svelte
+++ src/routes/table/+page.svelte
@@ -1,38 +1,65 @@
 <script>
   import SvelteTable from "svelte-table";
 
-  import {switchData} from "$lib/switchData.js";
+  import { switchData } from "$lib/switchData.js";
   let keys = Object.keys(switchData[0]);
-  console.log(keys);
   let sortBy = "Brand";
   let sortOrder = 1;
   const rows = switchData;
   const columns = [
-    {key: "Brand", title: "Brand", value: v => v.Brand, sortable: true,},
-    {key: "Name", title: "Name", value: v => v.Name, sortable: true,},
-    {key: "Type", title: "Type", value: v => v.Type, sortable: true,},
-    {key: "ActuationForce", title: "Actuation Force", value: v => v.ActuationForce, sortable: true,},
-    {key: "ActuationDistance", title: "Actuation Distance", value: v => v.ActuationDistance, sortable: true,},
-    {key: "BottomOutForce", title: "Bottom Out Force", value: v => v.BottomOutForce, sortable: true,},
-    {key: "TravelDistance", title: "Bottom Out Travel", value: v => v.TravelDistance, sortable: true,},
-    {key: "StemType", title: "Stem Compatibility", value: v => v.StemType, sortable: true,},
-    {key: "PrebuiltAvailability", title: "Available in prebuilt", value: v => v.PrebuiltAvailability, sortable: true,}
-  ]
-  
+    {
+      key: "Brand",
+      title: "Brand",
+      value: (v) => v.Brand,
+      sortable: true,
+    },
+    {
+      key: "Name",
+      title: "Switch Name",
+      value: (v) => v.Name,
+      sortable: true,
+    },
+    {
+      key: "Type",
+      title: "Type",
+      value: (v) => v.Type,
+      sortable: true,
+    },
+    {
+      key: "ActuationForce",
+      title: "Actuation Force (g)",
+      value: (v) => v.ActuationForce,
+      sortable: true,
+    },
+    {
+      key: "BottomOutForce",
+      title: "Bottom Out Force (g)",
+      value: (v) => v.BottomOutForce,
+      sortable: true,
+    },
+    {
+      key: "StemType",
+      title: "Stem Compatibility",
+      value: (v) => v.StemType,
+      sortable: true,
+    },
+  ];
 </script>
 
-  <div>
-    <SvelteTable
-      columns={columns}
-      rows={rows}
-      bind:sortBy
-      bind:sortOrder
-      classNameTable={['table table-striped']}
-      classNameThead={['table-primary']}
-      classNameSelect={['custom-select']}/>
+<div class="row">
+  <SvelteTable
+    {columns}
+    {rows}
+    bind:sortBy
+    bind:sortOrder
+    classNameTable={["table table-striped"]}
+    classNameThead={["table-primary"]}
+    classNameSelect={["custom-select"]}
+  />
 </div>
 
-
 <style>
-  
+  .row {
+    margin: 0 10rem;
+  }
 </style>
