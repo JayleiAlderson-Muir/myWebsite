5346206d9edf010bdbcf7a5bbf9fd155048fbb74
JayleiAlderson-Muir <jamet809jay@gmail.com>
2023-06-23 19:54:10 +1200

    feat : add solution to using #about on filtered table

--- src/routes/filtered/+page.svelte
+++ src/routes/filtered/+page.svelte
@@ -1,13 +1,19 @@
 <script>
-    import TableDisplay from "$lib/tableDisplay.svelte";
-    import { switchData } from "$lib/switchData";
-    import { page } from "$app/stores";
+  import TableDisplay from "$lib/tableDisplay.svelte";
+  import { switchData } from "$lib/switchData";
+  import { page } from "$app/stores";
 
-    let filteredRows = switchData.filter((x) =>
-        $page.url.hash.replace("#", "") === ""
-            ? x
-            : x.Brand == $page.url.hash.replace("#", "")
+  let filteredRows;
+  //filter table row data by hash value in url
+  if ($page.url.hash !== "#about") {
+    filteredRows = switchData.filter((x) =>
+      $page.url.hash.replace("#", "") === ""
+        ? x
+        : x.Brand == $page.url.hash.replace("#", "")
     );
+  } else {
+    window.location.href = "/table#about"; //changes page as table would no longer be filtered
+  }
 </script>
 
 <TableDisplay rows={filteredRows} />
